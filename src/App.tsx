ecaf31de341654316c0ac7915b48ed8d47d39525
hienphan0111 <hienphan@digitalcad.edu.vn>
2023-04-27 16:25:12 +0700

    Add AddtoList component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import List from './components/List';
+import AddToList from './components/AddToList';
 
 interface IState {
   people: {
@@ -25,6 +26,7 @@ function App() {
   return (
     <div className="App">
       <List people={people} />
+      <AddToList />
     </div>
   );
 }
---
+++ src/components/AddToList.tsx
@@ -0,0 +1,22 @@
+const AddToList = () => {
+  return (
+    <div>
+        <input type="text"
+            placeholder="name"
+            className="AddToList-input"
+        />
+
+        <input type="text"
+            placeholder="Age"
+            className="AddToList-input"
+        />
+        <input type="text"
+            placeholder="Note"
+            className="AddToList-input"
+        />
+        Add To List
+    </div>
+  )
+};
+
+export default AddToList;
